d3b1b99f20cb88879f03d9419f333dcf49596e86
Ayra Hikari <AyraHikari@gmail.com>
2019-04-01 19:56:55 +0700

    Added guide for Heroku users
    Signed-off-by: Ayra Hikari <AyraHikari@gmail.com>

--- README.md
+++ README.md
@@ -10,6 +10,33 @@ And then run the bot by `python bot.py`
 
 Done, now you can send message to your bot
 
+## For Heroku User
+1. Fork this git
+
+2. Go to heroku, and create new project. Select Python 3 if needed.
+
+3. After you created project, go to Deploy tab
+
+4. Select connect github
+
+5. Search for SimpeTelegramBot and click Connect button
+
+6. After connected, go to settings tab, press Reveal Config Vars, then insert like this
+
+`Var: Yes`
+
+`TokenBot: Your bot token from @botfather`
+
+`MyID: Insert your user id`
+
+7. Then finally, go to Resources tab, click Pen/Pencil button, turn on it, then Confirm
+
+8. Check your bot on telegram
+
+- For get user id, [Check this bot](t.me/EmiliaHikariBot) then type `/id`
+
+- If you need help, [Join my group](https://t.me/BotPythonIndonesia)
+
 ## Example script usage
 
 `/start` bot will reply `Hai!`
--- bot.py
+++ bot.py
@@ -11,8 +11,17 @@ from telegram.ext import Filters
 # Enable logging
 logging.basicConfig(level=logging.INFO)
 
-TOKEN = "Token" # Your bot token from @botfather
-CreatorID = 0000000 # Your ID
+TOKEN = "Insert your bot token here" # Your bot token from @botfather
+CreatorID = 0 # Your ID, see README.md for get your ID
+
+# Use variable system instead if True
+Variable = bool(os.environ.get('Var', False))
+if Variable:
+	TOKEN = os.environ.get('TokenBot', False)
+	CreatorID = os.environ.get('MyID', False)
+else:
+	TOKEN = TOKEN
+	CreatorID = CreatorID
 
 bot = telegram.Bot(token=TOKEN)
 updater = Updater(token=TOKEN)
@@ -29,10 +38,10 @@ def SendToCreator(bot, update):
 	name = update.effective_message.from_user.first_name
 	msg = update.effective_message
 	text = update.effective_message.text
-	# Replace method
-	# msg = msg.replace("/feedback ", "")
-	# Split method
-	text = text.split(None, 1)[1]
+	# Use replace method 
+	# msg = msg.replace("/feedback ", "") # Disabled
+	# Use split method
+	text = text.split(None, 1)[1] # Enabled
 	chat_id = update.effective_chat.id
 	message = "*New* `1` message from [{name}](tg://user?id={id})\n\n{message}".format(name=name, \
 		id=msg.from_user.id, message=text)
